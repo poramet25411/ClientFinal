570787f3a68ee1c8ae7bac1f16aea62e03dfc995
poramet25411 <koomuda37@gmail.com>
2022-04-26 20:52:05 +0700

    4

--- frontend/pages/index.js
+++ frontend/pages/index.js
@@ -68,20 +68,20 @@ import Link from 'next/link'
         <Navbar />
         <br></br>
 
-        <h1>LittlepigShop</h1>
+        <h1>LIVERPOOL FC</h1>
         <h1>welcome</h1>
         <link rel="preconnect" href="https://fonts.gstatic.com"/>
 <link href="https://fonts.googleapis.com/css2?family=Itim&display=swap" rel="stylesheet"></link>
 
 
-        <img  src="https://scontent.fbkk14-1.fna.fbcdn.net/v/t1.6435-9/p843x403/175096253_2893848187520098_2695457136069938488_n.jpg?_nc_cat=100&ccb=1-3&_nc_sid=8bfeb9&_nc_ohc=MhrCpVD3CUsAX8matxO&_nc_ht=scontent.fbkk14-1.fna&tp=6&oh=206d6df77fa83dcbf9f47a6477fb2698&oe=60A32CB6" alt="Trulli" width="500" height="500"></img>
+        <img  src="https://upload.wikimedia.org/wikipedia/en/thumb/0/0c/Liverpool_FC.svg/1200px-Liverpool_FC.svg.png" alt="Trulli" width="500" height="500"></img>
 <p>
-        <a href="https://www.facebook.com/"><img  src="http://icons.iconarchive.com/icons/paomedia/small-n-flat/1024/social-facebook-icon.png"  width="50" height="50" ></img></a>
+        <a href="https://www.facebook.com/ThailandLiverpoolFC/?brand_redir=67920382572"><img  src="http://icons.iconarchive.com/icons/paomedia/small-n-flat/1024/social-facebook-icon.png"  width="50" height="50" ></img></a>
 
-        <a href="https://www.instagram.com/littlepig.shopp/?igshid=1oij3vr1kxeu4&fbclid=IwAR04ecKAJjnfKKMAwLHBQ4ckYyFrru4Ksgs4jvIgNvygAtUInAqu18-82Vk">
-          <img  src="https://news.artnet.com/app/news-upload/2018/09/2000px-Instagram_logo_2016.svg_.png"  width="50" height="50" ></img></a>      
+        <a href="https://www.youtube.com/c/LiverpoolFC">
+          <img  src="https://play-lh.googleusercontent.com/lMoItBgdPPVDJsNOVtP26EKHePkwBg-PkuY9NOrc-fumRtTFP4XhpUNk_22syN4Datc"  width="50" height="50" ></img></a>      
 
-          <a href="https://line.me/R/ti/p/%40zhv2221x"><img  src="https://cdn.icon-icons.com/icons2/846/PNG/512/Line_icon-icons.com_67183.png"  width="50" height="50" ></img></a>   
+           
         </p>
         
         
